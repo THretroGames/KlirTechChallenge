11a71d9739c450d26e3de68b9f2d25441b102f72
THretroGames <48962240+THretroGames@users.noreply.github.com>
2022-01-18 18:23:42 -0300

    Walking Skeleton Angular

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = "KlirTechChallenge";
+  products: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.http.get("http://localhost:5000/api/products").subscribe(
+      (response) => {
+        this.products = response;
+        console.log(this.products);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
